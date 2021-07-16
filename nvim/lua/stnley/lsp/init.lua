cdf787f1bcf38c0e0ff00285a645bc0a6b933a90
stnley <64174376+stnley@users.noreply.github.com>
2021-07-15 23:23:31 -0400

    added efm-language-server and formatters to vim

--- nvim/lua/stnley/lsp/init.lua
+++ nvim/lua/stnley/lsp/init.lua
@@ -2,12 +2,58 @@ require "stnley.lsp.handlers"
 
 local lspconfig = require("lspconfig")
 
-local function on_attach()
-    -- smarter people than me said to do this
-    -- i will follow blindly
+local custom_init = function(client)
+    client.config.flags = client.config.flags or {}
+    client.config.flags.allow_incremental_sync = true
 end
 
-lspconfig.pyright.setup { on_attach = on_attach }
+local custom_attach = function(client)
+    -- Set autocommands conditional on server_capabilities
+    if client.resolved_capabilities.document_formatting then
+        vim.cmd [[augroup Format]]
+        vim.cmd [[autocmd! * <buffer>]]
+        vim.cmd [[autocmd BufWritePost <buffer> lua vim.lsp.buf.formatting()]]
+        vim.cmd [[augroup END]]
+    end
 
-lspconfig.tsserver.setup { on_attach = on_attach }
+    if client.resolved_capabilities.document_highlight then
+        -- nvim_exec [[
+        -- augroup lsp_document_highlight
+        -- autocmd! * <buffer>
+        -- autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+        -- autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+        -- augroup END
+        -- ]]
+        vim.cmd [[augroup lsp_document_highlight]]
+        vim.cmd [[autocmd! * <buffer>]]
+        vim.cmd [[autocmd CursorHold <buffer> :lua vim.lsp.buf.document_highlight()]]
+        vim.cmd [[autocmd CursorMoved <buffer> :lua vim.lsp.buf.clear_references()]]
+        vim.cmd [[augroup END]]
+    end
+end
+
+lspconfig.pyright.setup {
+    on_init = custom_init,
+    on_attach = custom_attach
+}
+
+lspconfig.tsserver.setup {
+    on_init = custom_init,
+    on_attach = custom_attach
+}
+
+local black = require "stnley.lsp.efm.black"
+local flake8 = require "stnley.lsp.efm.flake8"
+local mypy = require "stnley.lsp.efm.mypy"
+lspconfig.efm.setup {
+    on_attach = custom_attach,
+    init_options = { documentFormatting = true },
+    root_dir = vim.loop.cwd,
+    settings = {
+        rootMarkers = {".git/"},
+        languages = {
+            python = { black, flake8, mypy }
+        }
+    }
+}
 
