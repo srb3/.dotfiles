9f6b440a5a1f2802b606b8bdaaee43cd00b899da
stnley <64174376+stnley@users.noreply.github.com>
2021-07-23 11:44:29 -0400

    removed poetry config. Pyright works better with an external venv directory instead of a local project one

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@
 
 source ./zsh/zshenv
 
-
 source $DOTFILES/install/install-zsh.sh
 source $DOTFILES/install/install-fonts.sh
 source $DOTFILES/install/install-X11.sh
@@ -10,5 +9,4 @@ source $DOTFILES/install/install-i3.sh
 source $DOTFILES/install/install-git.sh
 source $DOTFILES/install/install-nvim.sh
 source $DOTFILES/install/install-tmux.sh
-source $DOTFILES/install/install-pypoetry.sh
 
--- install/install-pypoetry.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mkdir -p $XDG_CONFIG_HOME/pypoetry
-
-ln -sf $DOTFILES/pypoetry/config.toml $XDG_CONFIG_HOME/pypoetry
-
