5fb0e847857d19b2bd864141e47747de51208987
stnley <64174376+stnley@users.noreply.github.com>
2021-08-24 00:58:10 -0400

    add cd functions using fzf

--- zsh/.config/zsh/scripts/scripts.zsh
+++ zsh/.config/zsh/scripts/scripts.zsh
@@ -1,3 +1,17 @@
 pyclean() {
     find . -regex '^.*\(__pycache__\|\.py[co]\)$' -delete
 }
+
+# fd - cd to selected directory
+fd() {
+  local dir
+  dir=$(find ${1:-.} -path '*/\.*' -prune \
+                  -o -type d -print 2> /dev/null | fzf +m) &&
+  cd "$dir"
+}
+
+# fda - including hidden directories
+fda() {
+  local dir
+  dir=$(find ${1:-.} -type d 2> /dev/null | fzf +m) && cd "$dir"
+}
