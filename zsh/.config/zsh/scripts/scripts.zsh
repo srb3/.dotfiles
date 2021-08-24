ac2c33b1fba1718d1f2966dd6e8e39ee0237b8e9
stnley <64174376+stnley@users.noreply.github.com>
2021-08-24 14:27:13 -0400

    add fuzzy find file function

--- zsh/.config/zsh/scripts/scripts.zsh
+++ zsh/.config/zsh/scripts/scripts.zsh
@@ -2,16 +2,31 @@ pyclean() {
     find . -regex '^.*\(__pycache__\|\.py[co]\)$' -delete
 }
 
+
 # fd - cd to selected directory
 fd() {
-  local dir
-  dir=$(find ${1:-.} -path '*/\.*' -prune \
-                  -o -type d -print 2> /dev/null | fzf +m) &&
-  cd "$dir"
+    local dir
+    dir=$(find ${1:-.} -path '*/\.*' -prune \
+        -o -type d -print 2> /dev/null | fzf +m) &&
+        cd "$dir"
 }
+
 
 # fda - including hidden directories
 fda() {
-  local dir
-  dir=$(find ${1:-.} -type d 2> /dev/null | fzf +m) && cd "$dir"
+    local dir
+    dir=$(find ${1:-.} -type d 2> /dev/null | fzf +m) && cd "$dir"
+}
+
+
+# ff - find a file and open it in your editor
+ff() {
+    if [ -n "${EDITOR+1}" ]; then
+        editor="$EDITOR"
+    else
+        echo "No \$EDITOR set for session"
+        return 1
+    fi
+    local file
+    file=$(find ${1:-.} -type f 2> /dev/null | fzf +m) && "$editor" "$file"
 }
