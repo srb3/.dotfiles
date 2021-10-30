aa1160bed5d1c2878a8e1146ef5c340644b515b9
stnley <64174376+stnley@users.noreply.github.com>
2021-10-30 01:43:46 -0400

    add zoom from aur

---
+++ install/90-install-zoom.sh
@@ -0,0 +1,33 @@
+#!/bin/zsh
+
+set -u
+set -o pipefail
+
+function aur_install() {
+	_target=$1
+	repo=$2
+    if [ -d "$_target/$repo" ]; then
+        echo "$repo already found."
+    else
+        echo "=============== INSTALLING $repo ==============="
+        git -C $_target clone "https://aur.archlinux.org/${repo}.git"
+        echo "CLONED $repo"
+    fi
+
+    if [ $? -eq 0 ]; then
+        set -e
+	    cd "$_target/$repo"
+        git pull
+	    makepkg -srcif --needed
+        echo "INSTALLED $repo"
+        return 0
+    else
+        echo "FAILED TO INSTALL $repo"
+        return 1
+    fi
+}
+
+
+target="$HOME/apps"
+
+aur_install $target zoom
