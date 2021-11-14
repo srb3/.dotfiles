40955707753fc29bdb1bd60bd4c3bf92b943dae5
stnley <64174376+stnley@users.noreply.github.com>
2021-11-14 18:36:59 -0500

    I thought I already had this (manual dl binary) but turns out not. So, installing from AUR

---
+++ install/90-install-terraformls.zsh
@@ -0,0 +1,49 @@
+#!/bin/zsh
+
+set -u
+set -o pipefail
+
+function yes_or_no {
+    while true; do
+        read "?$* [y/n]: " yn
+        case $yn in
+            [Yy]*)
+                return 0
+                ;;
+            [Nn]*)
+                echo "Cancelled"
+                return  1
+                ;;
+        esac
+    done
+}
+
+function aur_install() {
+	_target=$1
+	repo=$2
+    if [ -d "$_target/$repo" ]; then
+        echo "$repo already found."
+    else
+        echo "=============== INSTALLING $repo ==============="
+        git -C $_target clone "https://aur.archlinux.org/${repo}.git"
+        echo "CLONED $repo"
+    fi
+
+    if [ $? -eq 0 ]; then
+        set -e
+	    cd "$_target/$repo"
+        git pull
+	    makepkg -srcif --needed
+        echo "INSTALLED $repo"
+        return 0
+    else
+        echo "FAILED TO INSTALL $repo"
+        return 1
+    fi
+}
+
+yes_or_no "Install terraform-ls?"
+if [ $? -eq 0 ]; then
+    target="$HOME/apps"
+    aur_install $target terraform-ls
+fi
